4941c1ee384761098edc112ade0421b7bc04be3f
Simon Cardona <simon.cdna@proton.me>
2026-01-03 12:19:40 +0100

    feat: add about page

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const EMAIL = "simon.cdna-pro@proton.me";
+export const GITHUB_URL = "https://github.com/simoncdn";
+export const GITHUB_BLOG_URL = "https://github.com/simoncdn/blog";
+export const LINKEDIN_URL = "https://www.linkedin.com/in/simoncdna/";
+export const PORTFOLIO_URL = "https://simoncardona.netlify.app/";
